64d746b20d5231acac99061385de2fbd75577070
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-05-13 10:24:50 +0530

    Initial Commit

---
+++ ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by mac on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
